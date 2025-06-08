066b56ac95ac5f0db28ed0b5a0e53ba7db2bccbe
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2025-06-08 20:16:38 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    print(f"Python version: `{sys.version}`")
+    print(f"This is executed using: `{sys.executable}`")
+    print("This is a test file")
+
+if __name__ == "__main__":
+    main()
